18f9cf620b5cf6b10945f7a1ccb8dc6f1e5856c1
Cheng-Dao <62025305+Cheng-Dao@users.noreply.github.com>
2020-03-14 13:24:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3.5 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib linux-libc-dev
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 #sed -i 's/OpenWrt/Silime/g' package/base-files/files/bin/config_generate
 
